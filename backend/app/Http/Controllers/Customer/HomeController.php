04ce775aec9af53c3129634c50c6c65d7867ae7b
Nego Thales <thale.serra97@gmail.com>
2020-06-30 12:02:17 -0300

    iniciando estil home

--- backend/app/Http/Controllers/Customer/HomeController.php
+++ backend/app/Http/Controllers/Customer/HomeController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('customer.pages.home.index');
+        $promotions = Product::get();
+        return view('customer.pages.home.index')
+            ->with('promotions', $promotions);
     }
 }
